1b981f80b2bfa8e3e184b51fac4c40a98d939dc5
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-08-27 09:01:48 -0400

    initials commits on local env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Bienvenidas a una App React creada con con el gestor yarn.
         </p>
         <a
           className="App-link"
